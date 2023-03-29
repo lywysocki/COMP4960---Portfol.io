a4a8a77d234f59ed3f4ae6b82e0468f1b8c41a73
sullivanb13atwit <55287471+sullivanb13atwit@users.noreply.github.com>
2023-03-28 21:26:42 -0400

    Added comments for examples to pull and manipulate data from retrieve_stock_data function.

--- database.py
+++ database.py
@@ -46,7 +46,7 @@ def is_valid_date(date):
 def get_input():
     ticker = input("Enter the stock ticker: ")
     while ticker not in stock_tickers:
-        ticker = input("We do not have data for that stock in our  : ")
+        ticker = input("We do not have data for that stock in our database: ")
 
     historical_date = input("What date would you like data to date back to (DD-MM-YYYY): ")
 
@@ -61,6 +61,27 @@ def get_input():
 # stock param is the ticker input from the user
 # start_date (MM-DD-YYYY) param is the date of how far the user wants to pull historical data from
 # return a dataframe containing the historical stock prices
+# the dataframe columns are Date, Open, High, Low, Close
+
+# example:
+# retrieve_stock_prices('AAPL', '01-01-2018')
+# returns a dataframe from January 1st, 2018 to current date containing the open, high, low, and close price for the
+# input stock for each day
+#             Date      Open     Close      High       Low
+# 0     2018-01-01   42.5400   43.0650   43.0750   42.3150
+# 1     2018-01-02   43.1325   43.0575   43.6375   42.9900
+# 2     2018-01-03   43.1350   43.2575   43.3675   43.0200
+# 3     2018-01-04   43.3600   43.7500   43.8425   43.2625
+# 4     2018-01-07   43.5875   43.5875   43.9025   43.4825
+
+# to manipulate the data:
+# mydf = retrieve_stock_prices('AAPL', '01-01-2018')
+# close_price = mydf['Close']
+
+# to loop through dataframe:
+# for i in range (len(mydf)):
+#   each_close_price = mydf.iloc[i]['Close']
+#   print(each_close_price)
 def retrieve_stock_prices(stock, start_date):
     # split the input date for month, day, year retrieval
     date_string = start_date.split('-')
@@ -104,16 +125,15 @@ def retrieve_stock_prices(stock, start_date):
         for timestamp in ts:
             date_string = str(datetime.fromtimestamp(timestamp))
             date_to_add = date_string.split(' ')
-            sub_date_to_add = date_to_add[0].split('-')
-            years += [sub_date_to_add[0]]
-            months += [sub_date_to_add[1]]
-            days += [sub_date_to_add[2]]
+            # sub_date_to_add = date_to_add[0].split('-')
+            # years += [sub_date_to_add[0]]
+            # months += [sub_date_to_add[1]]
+            # days += [sub_date_to_add[2]]
+            dates += [date_to_add[0]]
 
         # create dictionary to populate dataframe
         data = {
-            'Year': years,
-            'Month': months,
-            'Day': days,
+            'Date': dates,
             'Open': open,
             'Close': close,
             'High': high,
@@ -166,3 +186,6 @@ def market_data(stock):
     df = pd.DataFrame(data, index=[0])
 
     return df
+
+
+print(retrieve_stock_prices('AAPL', '01-01-2018')['Close'])
