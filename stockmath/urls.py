4471c07d8b9bc77830cc3b8671183d110d6e678b
polischukjatwit <polischukj@wit.edu>
2023-03-30 20:10:11 -0400

    Completed base webpage

--- stockmath/urls.py
+++ stockmath/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base_page),
+    path('', views.base_page, name='index'),
 ]
--- stockmath/views.py
+++ stockmath/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from .forms import InputForm
 
 # Create your views here.
 
 def base_page(request):
-    return render(request, 'index.html')
+    # request has a filled form
+    if request.method == 'POST':
+        # "bind" the form data
+        form = InputForm(request.POST)
+        if form.is_valid():
+            context = dict(**form.cleaned_data)
+            context['form'] = form
+            # DATA FETCHING BASED ON FORM INPUT GOES HERE
+            return render(request, 'index.html', context)
+    else:
+        form = InputForm()
+    
+    return render(request, 'index.html', {'form': form})
