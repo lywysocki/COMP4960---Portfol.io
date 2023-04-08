fbd11994556f861e6f89a188834d55fa9de8d37b
polischukjatwit <polischukj@wit.edu>
2023-04-08 11:05:46 -0400

    webpage complete

--- stockmath/views.py
+++ stockmath/views.py
@@ -14,8 +14,19 @@ def base_page(request):
             context['form'] = form
             context['ticker'] = form.cleaned_data['ticker'].upper()
             # DATA FETCHING BASED ON FORM INPUT GOES HERE
-            return render(request, 'index.html', context)
+            context['open'] = 20
+            context['close'] = 30
+            context['high'] = 40
+            context['low'] = 10
+            context['pred_conf'] = 80
     else:
         form = InputForm()
+        context = {}
+        context['form'] = form
+        context['open'] = "--"
+        context['close'] = "--"
+        context['high'] = "--"
+        context['low'] = "--"
+        context['pred_conf'] = "X"
     
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html', context)
